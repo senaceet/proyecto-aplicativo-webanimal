ff6d55235a7f5b501c4446865b8cdf9aa657365b
j0hanZ4pata <69684303+j0hanZ4pata@users.noreply.github.com>
2022-03-22 08:06:27 -0500

    docs: nosotros actualizado

--- Proyecto/mysite/pets/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('search', views.PetsSearchListView.as_view(), name='search'),
-    path('<slug:slug>', views.PetsDetailView.as_view(), name='pets'),
-]
